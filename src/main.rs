62ad290142721918051c247cab0bac32b0dd6e48
catid <mrcatid@gmail.com>
2019-01-06 21:21:50 -0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,157 @@
+#![recursion_limit="256"]
+
 #[macro_use]
 extern crate stdweb;
+extern crate nalgebra_glm as glm;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use stdweb::unstable::TryInto;
+//use stdweb::web::INonElementParentNode;
+
+struct AudioState {
+    js_ctx: stdweb::Value,
+    audio_buffer: Vec<f32>,
+    last_timestamp: f64,
+}
+
+// Buffer an audio buffer sample to the given channel
+fn js_buffer_audio(js_ctx: &stdweb::Value, channel: u32, sample_buffer: &Vec<f32>) -> f64 {
+    let result : f64 = js! {
+        var channel = @{channel};
+        var h = @{js_ctx};
+        var samples = @{unsafe { stdweb::UnsafeTypedArray::new(sample_buffer) }};
+
+        var sample_count = samples.length;
+        var sample_rate = 48000;
+        var latency = 1500.0 / sample_rate;
+
+        var audio_buffer;
+        if (h.empty_audio_buffers.length === 0) {
+            audio_buffer = h.audio.createBuffer(1, sample_count, sample_rate);
+        } else {
+            audio_buffer = h.empty_audio_buffers.pop();
+        }
+        audio_buffer.getChannelData(channel).set(samples);
+
+        var node = h.audio.createBufferSource();
+        node.connect(h.audio.destination);
+        node.buffer = audio_buffer;
+        node.onended = function() {
+            h.empty_audio_buffers.push(audio_buffer);
+        };
+
+        var buffered = h.play_timestamp - (h.audio.currentTime + latency);
+        var play_timestamp = Math.max(h.audio.currentTime + latency, h.play_timestamp);
+        node.start(play_timestamp);
+        h.play_timestamp = play_timestamp + sample_count / sample_rate;
+
+        return buffered;
+    }.try_into().unwrap();
+    js! {
+        console.log("Buffered audio: " + @{result});
+    };
+    return result;
+}
+
+impl AudioState {
+    fn new() -> AudioState {
+        let element_audio = js! {
+            return {
+                audio: new AudioContext(),
+                empty_audio_buffers: [],
+                play_timestamp: 0.0,
+            };
+        };
+
+        AudioState {
+            js_ctx: element_audio,
+            audio_buffer: Vec::new(),
+            last_timestamp: 0.0,
+        }
+    }
+    fn add_sample(&mut self, sample: f32) {
+        self.audio_buffer.push(sample);
+        if self.audio_buffer.len() >= 2048 {
+            js_buffer_audio(
+                &self.js_ctx,
+                0,
+                &self.audio_buffer);
+            self.audio_buffer.clear();
+        }
+    }
+
+    fn update(&mut self, timestamp: f64) {
+        let delta_msec = timestamp - self.last_timestamp;
+        self.last_timestamp = timestamp;
+
+        js! {
+            console.log("Audio frame: delta_msec=" + @{delta_msec});
+        };
+
+        for x in 0..100 {
+            let t = timestamp + (x as f64);
+            let s = (t * 3.14159 * 2.0) as f32;
+            self.add_sample(s.sin());
+        }
+    }
+}
+
+
+struct RenderState {
+    last_timestamp: f64,
+}
+
+impl RenderState {
+    fn draw(&mut self, timestamp: f64) {
+        let _delta_msec = timestamp - self.last_timestamp;
+        self.last_timestamp = timestamp;
+    }
+}
+
+
+struct GameLoop {
+    render: RenderState,
+    audio: AudioState,
+}
+
+impl GameLoop {
+    fn new() -> GameLoop {
+        //let element_canvas = stdweb::web::document().get_element_by_id("game_canvas").unwrap();
+
+        GameLoop {
+            audio: AudioState::new(),
+            render: RenderState {
+                last_timestamp: 0.0,
+            },
+        }
+    }
+}
+
+fn render_loop(looper: Rc<RefCell<GameLoop>>) {
+    stdweb::web::window().request_animation_frame(move |timestamp: f64| {
+        {
+            let mut mlooper = looper.borrow_mut();
+            mlooper.render.draw(timestamp);
+            mlooper.audio.update(timestamp);
+        }
+        render_loop(looper);
+    });
+}
+
+
+#[js_export]
+fn handle_play_click(_e: stdweb::Value) {
+    js! {
+        console.log("TEST");
+    };
+
+    let looper = Rc::new(RefCell::new(GameLoop::new()));
+    render_loop(looper);
+}
+
 
 fn main() {
     stdweb::initialize();
-
-    let message = "Hello World";
-    let result = js! {
-        console.log( @{message} );
-        return 2;
-    };
-    js! {
-        console.log( @{result} );
-        return 2;
-    };
-
     stdweb::event_loop();
 }
--- src/runtime.js
+++ src/runtime.js
@@ -1,1 +1,6 @@
 // This file is prepended to the Javascript produced by Cargo-Web
+
+var play = document.getElementById('play_button');
+play.onclick = function(e) {
+    Module.exports.handle_play_click(e);
+};
