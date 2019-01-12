01f88faabcd1aeb8683f67896e8d13761d9c8969
catid <mrcatid@gmail.com>
2019-01-11 23:37:13 -0800

    Beeps

--- src/main.rs
+++ src/main.rs
@@ -5,109 +5,111 @@ use std::rc::Rc;
 
 #[macro_use]
 extern crate stdweb;
-use stdweb::unstable::TryInto;
+//use stdweb::unstable::TryInto;
 
 extern crate nalgebra_glm as glm;
 
 extern crate sample;
-use sample::{Signal, Sample, Frame};
+use sample::Signal;
 
 //use stdweb::web::INonElementParentNode;
 
-struct AudioState {
-    js_ctx: stdweb::Value,
-    audio_buffer: Vec<f32>,
-    last_timestamp: f64,
-}
 
 // Buffer an audio buffer sample to the given channel
-fn js_buffer_audio(js_ctx: &stdweb::Value, channel: u32, sample_buffer: &Vec<f32>) -> f64 {
-    let result : f64 = js! {
-        var channel = @{channel};
+fn js_play_buffer(js_ctx: &stdweb::Value, sample_buffer: &Vec<f32>) {
+    js! {
         var h = @{js_ctx};
         var samples = @{unsafe { stdweb::UnsafeTypedArray::new(sample_buffer) }};
 
         var sample_count = samples.length;
         var sample_rate = 48000;
-        var latency = 1500.0 / sample_rate;
 
-        var audio_buffer;
-        if (h.empty_audio_buffers.length === 0) {
-            audio_buffer = h.audio.createBuffer(1, sample_count, sample_rate);
-        } else {
-            audio_buffer = h.empty_audio_buffers.pop();
-        }
-        audio_buffer.getChannelData(channel).set(samples);
+        var audio_buffer = h.audio.createBuffer(1, sample_count, sample_rate);
+
+        audio_buffer.getChannelData(0).set(samples);
 
         var node = h.audio.createBufferSource();
         node.connect(h.audio.destination);
         node.buffer = audio_buffer;
-        node.onended = function() {
-            h.empty_audio_buffers.push(audio_buffer);
-        };
 
-        var buffered = h.play_timestamp - (h.audio.currentTime + latency);
-        var play_timestamp = Math.max(h.audio.currentTime + latency, h.play_timestamp);
+        var latency = 0.1;
+        var play_timestamp = h.audio.currentTime + latency;
         node.start(play_timestamp);
-        h.play_timestamp = play_timestamp + sample_count / sample_rate;
+    }
+}
 
-        return buffered;
-    }.try_into().unwrap();
-    js! {
-        console.log("Buffered audio: " + @{result});
-    };
-    return result;
+const SAMPLE_RATE: usize = 48_000;
+const SAMPLE_HZ: f64 = SAMPLE_RATE as f64;
+const BEEP_SAMPLES: usize = SAMPLE_RATE/10;
+
+struct AudioState {
+    js_ctx: stdweb::Value,
+    old_timestamp: f64,
+    beep_a: Vec<f32>,
+    beep_b: Vec<f32>,
+    beep_c: Vec<f32>,
 }
 
 impl AudioState {
     fn new() -> AudioState {
         let element_audio = js! {
             return {
-                audio: new AudioContext(),
-                empty_audio_buffers: [],
-                play_timestamp: 0.0,
+                audio: new AudioContext()
             };
         };
 
+        let signal0 = sample::signal::rate(SAMPLE_HZ).const_hz(300.0).square().scale_amp(0.05);
+        let signal1 = sample::signal::rate(SAMPLE_HZ).const_hz(400.0).square().scale_amp(0.05);
+        let signal2 = sample::signal::rate(SAMPLE_HZ).const_hz(500.0).sine().scale_amp(0.05);
+        let buffer_a: Vec<_> = signal0.add_amp(signal1).add_amp(signal2)
+            .take(BEEP_SAMPLES)
+            .map(|x|{x[0] as f32}).collect();
+
+        let signal3 = sample::signal::rate(SAMPLE_HZ).const_hz(400.0).square().scale_amp(0.05);
+        let signal4 = sample::signal::rate(SAMPLE_HZ).const_hz(500.0).square().scale_amp(0.05);
+        let signal5 = sample::signal::rate(SAMPLE_HZ).const_hz(600.0).sine().scale_amp(0.05);
+        let buffer_b: Vec<_> = signal3.add_amp(signal4).add_amp(signal5)
+            .take(BEEP_SAMPLES)
+            .map(|x|{x[0] as f32}).collect();
+
+        let signal6 = sample::signal::rate(SAMPLE_HZ).const_hz(500.0).square().scale_amp(0.05);
+        let signal7 = sample::signal::rate(SAMPLE_HZ).const_hz(600.0).square().scale_amp(0.05);
+        let signal8 = sample::signal::rate(SAMPLE_HZ).const_hz(700.0).sine().scale_amp(0.05);
+        let buffer_c: Vec<_> = signal6.add_amp(signal7).add_amp(signal8)
+            .take(BEEP_SAMPLES)
+            .map(|x|{x[0] as f32}).collect();
+
         AudioState {
             js_ctx: element_audio,
-            audio_buffer: Vec::new(),
-            last_timestamp: 0.0,
-        }
-    }
-    fn add_sample(&mut self, sample: f32) {
-        self.audio_buffer.push(sample);
-        if self.audio_buffer.len() >= 2048 {
-            js_buffer_audio(
-                &self.js_ctx,
-                0,
-                &self.audio_buffer);
-            self.audio_buffer.clear();
+            old_timestamp: 0.0,
+            beep_a: buffer_a,
+            beep_b: buffer_b,
+            beep_c: buffer_c,
         }
     }
 
     fn update(&mut self, timestamp: f64) {
-        let delta_msec = timestamp - self.last_timestamp;
-        self.last_timestamp = timestamp;
+        let delta = timestamp - self.old_timestamp;
+        if delta < 1000.0 {
+            return;
+        }
+        self.old_timestamp = timestamp;
 
-        js! {
-            console.log("Audio frame: delta_msec=" + @{delta_msec});
-        };
+        let beep: &Vec<f32>;
 
-        let hz = sample::signal::rate(48_000.0).const_hz(440.0).sine();
-        let one_sec = 48_000 as usize;
-        let frame: Vec<_> = hz.take(one_sec).map(|x|{x[0] as f32}).collect();
+        if delta as u32 % 3 == 0 {
+            beep = &self.beep_a;
+        }
+        else if delta as u32 % 3 == 1 {
+            beep = &self.beep_b;
+        }
+        else {
+            beep = &self.beep_c;
+        }
 
-        js_buffer_audio(
+        js_play_buffer(
             &self.js_ctx,
-            0,
-            &frame);
-/*
-        for x in 0..100 {
-            let t = timestamp + (x as f64);
-            let s = (t * 3.14159 * 2.0) as f32;
-            self.add_sample(s.sin());
-        }*/
+            beep);
     }
 }
 
